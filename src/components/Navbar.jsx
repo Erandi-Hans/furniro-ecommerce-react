25d7d9f468533a34878877bf65e4da9262132446
G.M Erandi Hansika <erandi2287hansika@gmail.com>
2026-05-19 00:19:24 +0530

    feat(router): import react router components and navigation link setup

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import Logo from "../assets/hero.png";
 
 import { AiOutlineUser, AiOutlineSearch, AiOutlineHeart, AiOutlineShoppingCart } from 'react-icons/ai';
